5aac8be54e60fc32b236380c19605db90a6175cb
Ngan Nguyen <ngannguyen977@gmail.com>
2017-06-29 17:11:37 +0700

    29th06

--- mainScript.js
+++ mainScript.js
@@ -1,33 +1,10 @@
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $('.fixtop').css({
-               "position": "fixed",
-               "top": 0,
-               "left": 0,
-               "right": 0
-            });
-        } else {
-            $('.fixtop').css("position", "relative");
-        }
-    });
-    $(".dropdown").hover(            
-
-        function() {
-
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
-
-            $(this).toggleClass('open');        
-
-        },
-        function() {
-
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
-
-            $(this).toggleClass('open');       
-        }
-
-    );
-
-});
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if ($(this).scrollTop() > 100) {
+//             $('.fixtop').addClass('fixnav scroll fixed')
+//         } else {
+//             $('.fixtop').removeClass("fixnav scroll fixed");
+//         }
+//     });
+// });
